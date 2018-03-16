1e9c6795736875fc1f273895bf0a14e5d81a3323
Alexandr Pozhuev <32191131+SashaPozhuev1@users.noreply.github.com>
2018-03-16 11:51:40 +0300

    Update vector.cpp

--- sources/vector.cpp
+++ sources/vector.cpp
@@ -102,21 +102,41 @@ void vector_t::push_back(int value)
 void vector_t::pop_back()
 {
 	//
-	
+	if ( size_ - 1 > capacity_ / 4 ) {
+		size_--;
+	}
+	else {
+		int newsize_ = size_ - 1;
+		int newcapacity_ = capacity / 2;
+		int * newelements_ = new int [newcapacity_];	
+		
+		for ( std::size_t i = 0; i < newsize_; ++i ) {
+			newelements_[ i ] = elements_[ i ];
+		}
+		delete[] elements_;
+		size_ = newsize_;
+		capacity_ = newcapacity_;
+		elements_ = newelements_;
+	}
 	//
 }
 
 int & vector_t::operator [](std::size_t index)
 {
-	return elements_[0];
+	return elements_[index];
 }
 
 int vector_t::operator [](std::size_t index) const
 {
-	return 0;
+	return elements_[index];
 }
 
 bool operator !=(vector_t const & lhs, vector_t const & rhs)
 {
-	return true;
+	if ( lhs == rhs ) {
+		return false;	
+	}
+	else {
+		return true;
+	}
 }
